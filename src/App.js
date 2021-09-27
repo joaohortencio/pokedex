4480aabafbe9b5cbbbf9c81f05214ed7114ceb9a
joao <joao@mezzoplanejamento.com.br>
2021-09-27 19:51:23 -0300

    Pokedex for a Frontend Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './App.css';
+import NavBar from './components/layout/NavBar';
+import Dashboard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
+
+class App extends Component {
+
+  
+  render() {
+    return (
+      <Router
+        forceRefresh={true}
+      >
+        <div className="App">
+          <NavBar /> 
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
